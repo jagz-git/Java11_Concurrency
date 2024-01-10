20c93c34cc0a5e7fb945cdf4f7641d51dd04e42c
Jagadesh Narayanaswamy <javajagz24@gmail.com>
2024-01-10 16:42:41 +0000

    Sleep and Join method examples

--- src/main/java/org/learning/Main.java
+++ src/main/java/org/learning/Main.java
@@ -10,12 +10,24 @@ public class Main {
         CustomThread customThread = new CustomThread();
         customThread.start();
 
-        Thread newThread = new Thread(new RunThread());
-        newThread.start();
-
         //Using Lambda expression
         Thread t2 = new Thread(() -> System.out.println("Lambda Runnable!"));
         t2.start();
+
+        Thread newThread = new Thread(new RunThread());
+        newThread.start();
+//        newThread.interrupt();
+
+        try {
+            newThread.join(1500); //Remove 1500 and check you will see the difference
+//            based on sleep time i.e. the waiting and the time specified the thread will wait and continue since join is called after 1.5 sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Thread newThread1 = new Thread(new RunThread());
+        newThread1.start();
+
 
 //        Callable
 
--- src/main/java/org/learning/RunThread.java
+++ src/main/java/org/learning/RunThread.java
@@ -1,11 +1,21 @@
 package org.learning;
 
 
-public class RunThread implements Runnable{
+public class RunThread implements Runnable {
 
     @Override
-    public void run(){
-        System.out.println("Runnable Interface thread!");
+    public void run() {
+//        System.out.println("Runnable Interface thread!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Current thread id: " + Thread.currentThread().getId() + "\tRunThread run: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+                break; //If you don't break every time the thread is interrupted will show the stack trace
+            }
+        }
     }
 
 }

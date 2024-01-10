1ebc27fe219c27eeebe4ab22ce7a272362cc7aa6
Jagadesh Narayanaswamy <javajagz24@gmail.com>
2024-01-10 16:02:03 +0000

    Init

---
+++ src/main/java/org/learning/Main.java
@@ -0,0 +1,11 @@
+package org.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Thread!");
+        Thread t = new Thread(() -> System.out.println("Hi learners!\n" + "Runnable: " + Thread.currentThread().getName()));
+        t.start();
+        System.out.println("Main: " + Thread.currentThread().getId());
+
+    }
+}
